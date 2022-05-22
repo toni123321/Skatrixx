aca34bb604fddf45e411b27f2b4c7d2c96c0808b
Takev,Antonio A.M <tonitakev@gmail.com>
2022-05-22 14:46:32 +0200

    Fix the design of lobby peding requests

--- frontend-files/src/components/lobby/LobbyParticipant.js
+++ frontend-files/src/components/lobby/LobbyParticipant.js
@@ -19,10 +19,10 @@ function LobbyParticipant(props) {
     if(user.image !== undefined) {
       return (
         <div className='lobby-participant'>
-          { pending ? <p style={{backgroundColor : '#1e1e1e'}} id='lobby-participant-pending'>Pending</p> : ''}
-          <div style={{opacity : pending ? .65 : 1}}>
+          
+          <div>
             <img src={user.image}  referrerPolicy='no-referrer' alt='Loading...'/>
-            <p>{user.username}</p>
+            <p>{pending ? "Pending..." : user.username}</p>
           </div>
         </div>
       )
