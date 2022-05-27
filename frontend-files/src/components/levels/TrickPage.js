e3adbbfb09beadc2ee1770d1db7ece61460e1756
Joan <jokrastanov@gmail.com>
2022-05-27 17:17:30 +0200

    Timer ready

--- frontend-files/src/components/levels/TrickPage.js
+++ frontend-files/src/components/levels/TrickPage.js
@@ -11,35 +11,35 @@ const TrickPage = () => {
     const [ running, setRunning ] = useState( true )
     const [ reset, setReset ] = useState( false )
 
+   //new timer is loaded in a paused state, awaiting 'play' command
+   const mountPaused = () => {
+     setTimerExists( true )
+     setRunning( false )
+   }
+
     useEffect(() => {
       mountPaused()
     }, [])
 
 
-    //     useEffect(() => {
-    //     SpeechRecognition.startListening({continues:true})
-    //     console.log("listeing starts")
-    // }, [])
-
-  
-    //new timer is loaded in a paused state, awaiting 'play' command
-    const mountPaused = () => {
-      setTimerExists( true )
-      setRunning( false )
-    }
-
-    //Speech command
-    const {
-      transcript,
-      listening,
-      resetTranscript,
-      browserSupportsSpeechRecognition,
-    } = useSpeechRecognition();
+      useEffect(() => {
+        startListening()
+ }, [])
 
     const commands = [
       {
-        command: ["Start *"],
-        callback: ({command}) => setMessage({command}),
+        command: ["start", "begin", "go", "now"],
+        callback: ({command}) => setRunning(true),
+        setRunning: true
+      },
+      {
+        command: ["stop", "pause", "wait"],
+        callback: ({command}) => setRunning(false),
+        setRunning: true
+      },
+      {
+        command: ["reset", "restart", "again", "refresh"],
+        callback: ({command}) => setReset(true),
         setRunning: true
       },
       {
@@ -47,6 +47,13 @@ const TrickPage = () => {
         callback: ({resetTranscript}) => resetTranscript()
       }
     ]
+
+        //Speech command
+        const {
+          listening,
+          browserSupportsSpeechRecognition,
+        } = useSpeechRecognition( {commands} );
+        const startListening = () => SpeechRecognition.startListening({ continuous: true });
   
     if (!browserSupportsSpeechRecognition) {
       return <span>Browser doesn't support speech recognition.</span>;
@@ -58,15 +65,11 @@ const TrickPage = () => {
       <div className='timerContainer'>
       <div className='speechCommand'>
       <div>
-      <p className='speechText'>{transcript}</p>
-        {/* <p>Microphone: {listening ? 'on' : 'off'}</p> */}
         <i class="fas fa-microphone fa-2x" id="timerIcons" onClick={SpeechRecognition.startListening}> {listening ? 'on' : 'off'}</i>
-        {/* <button onClick={SpeechRecognition.startListening}>Start</button> */}
       </div>
       </div>
       {timerExists ? <Timer id={'timer'} running={running} setRunning={setRunning} reset={reset} setReset={setReset} fillColor={'rgba(166, 31, 31, 1)'} bgColor={'#101010'}/> : null }
         <div className={'iconsContainer'} style={{ display: 'flex' }}>
-          {/* <button onClick={() => setRunning( true )}>Play</button> */}
           <i class="fas fa-play fa-2x" id="timerIcons" onClick={() => setRunning( true )}></i>
           <i class="fa-solid fa-pause fa-2x" id="timerIcons" onClick={() => setRunning( false )}></i>
           <i class="fa-solid fa-undo fa-2x" id="timerIcons" onClick={() => setReset( true )}></i>
