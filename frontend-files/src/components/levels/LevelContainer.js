20d161f2dbc9d1201f8b1a15ee8ce1588bd1ae84
Sesil <s.tasim@student.fontys.nl>
2022-06-03 16:30:23 +0200

    Trick list new design changes

--- frontend-files/src/components/levels/LevelContainer.js
+++ frontend-files/src/components/levels/LevelContainer.js
@@ -24,22 +24,26 @@ function LevelContainer(props) {
   return (
       <div className='trick'>
         <div id='trick-header' onClick={openTrick}>
-        <p id="trickName">{props.trick.name}</p> 
+          <p className='trick-line-colored'>|</p>
+        <p id="trickName">{props.trick.name} - {props.trick.xp}XP</p> 
         <div className='trick-right'>
-        <p id="trickXp">{props.trick.xp}xp</p>
         <i className="material-icons play-trick-arrow">keyboard_arrow_right</i>
         </div>
         </div>
+        <div className='trick-container-detail'>
+          <p className='trick-minutes'>2,4 mins</p>
+          <p className='trick-avarge-scoretext'>Based on avarge score</p>
+        </div>
         <div className='trick-content'>
           <div id="VideoDiv">
-            <iframe
+            {/* <iframe
               id="videoFrame"
               src={props.trick.videoLink}
               frameborder="0"
               allow="autoplay; encrypted-media"
               allowFullScreen
               title="video"
-            />
+            /> */}
           </div>
           <button type="button" id="trickBtn" onClick={startTrick}>Start</button>
         </div>
