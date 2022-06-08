618ecbb07ceca44bf3e13709c027fa8a50625df1
Sesil <s.tasim@student.fontys.nl>
2022-06-08 18:00:25 +0200

    Created google maps basics

---
+++ find-by-location/src/Location.js
@@ -0,0 +1,22 @@
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import "./Location.css"
+
+export default function Loading() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
+}
+
+function Map() {
+  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+
+  return (
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+      <Marker position={center} />
+    </GoogleMap>
+  );
+}
